6b281881184c3c97228e3b4b679d2cab46a37c7a
Parth <parthsolanki.solanki@gmail.com>
2019-07-13 12:00:14 -0400

    Created login view

--- app/src/main/java/com/example/chronicplanner/MainActivity.java
+++ app/src/main/java/com/example/chronicplanner/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.chronicplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login, register;
+    private EditText password, username;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clickerFunction();
+    }
+
+    public void clickerFunction() {
+        login = (Button)findViewById(R.id.login_button);
+        register = (Button)findViewById(R.id.register);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+
+        login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(username.getText().toString().equals("ParthSolanki1") && password.getText().toString().equals("KanakSolanki1")){
+                            Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Incorrect Username or Password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+        register.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, registration.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+
     }
 }
